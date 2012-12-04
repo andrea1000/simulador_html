58e3764db183cc13b562595bfad3b7d4e7f4a171
Vladimir <vladimir_11_17@hotmail.com>
2012-12-04 15:32:38 -0500

    Ok

--- credito.php
+++ credito.php
@@ -3,88 +3,86 @@
 $cabecera = '<script src="Script/Script.js" type="text/javascript"></script>';
 include 'header.php';
 ?>
-<table class="form">
-    <tbody>
-        <tr>
-            <td class="label">
-                <label id="ddlProducto">Tipo Producto de Crédito</label>
-            </td>
-            <td class="input">
-                <div>
-                    <select id="ddlProducto">
-
+<div class="formulario">         
+ <table class="form">
+        <tbody>
+            <tr>
+                <td class="label">
+                    <label>Tipo Producto de Crédito</label>
+                </td>
+                <td class="input">
+                    <select>
+					
                     </select>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="label">
-                <label id="ddlSubProducto">Producto</label>
-            </td>
-            <td class="input">
-                <div>
-                    <select id="ddlSubProducto">
-
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Producto</label>
+                </td>
+                <td class="input">
+                    <select>
+					
                     </select>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="label">
-                <label>Agencia de Solicitud" AssociatedControlID="ddlAgencia" />
-            </td>
-            <td class="input">
-                <div>
-                    <select id="ddlAgencia">
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Agencia de Solicitud</label>
+                </td>
+                <td class="input">
+                    <select>
 
-                    </select>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="label">
-                <label>Monto" AssociatedControlID="txtMonto" />
-            </td>
-            <td class="input">
-                <input type="text" id="txtMonto" class="numerico"/>
-            </td>
-        </tr>
-        <tr>
-            <td class="label">
-                <label>Moneda" AssociatedControlID="rbtnMoneda" />
-            </td>
-            <td class="input">
-                <div>
-                    <select id="rbtnMoneda">
+					</select>
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Monto</label>
+                </td>
+                <td class="input">
+                    <input type="text" class="numerico" onkeypress="return isNumberKey(event)">
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Moneda</label>
+                </td>
+                <td class="input">
+                    <table border="0">
+	<tbody><tr>
+		<td><input type="radio" value="1" checked="checked"><label>Soles</label></td><td><input type="radio" value="2"><label>Dólares</label></td>
+	</tr>
+</tbody></table>
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Nro de Cuotas</label>
+                </td>
+                <td class="input">
+                    <input type="text" class="numerico" onkeypress="return isNumberKey(event)">
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Tasa General</label>
+                </td>
+                <td class="input">
+                    <select style="width: 156px;">
+					</select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="right">
+                    <input type="submit" value="Calcular" class="button">
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
-                    </select>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td class="label">
-                <label>Nro de Cuotas" AssociatedControlID="txtNroCuotas" />
-            </td>
-            <td class="input">
-                <input type="text" id="txtNroCuotas" class="numerico"/>
-            </td>
-        </tr>
-        <tr>
-            <td class="label">
-                <label>Tasa General" AssociatedControlID="ddlTasa" />
-            </td>
-            <td class="input">
-                <input type="text" id="ddlTasa" class="numerico"/>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2" align="right">
-                <input type="submit" class="button"/>
-            </td>
-        </tr>
-    </tbody>
-</table>
+    </div>
+
 <?php
-//hola2
 include 'footer.php';
 ?>
--- deposito.php
+++ deposito.php
@@ -14,7 +14,140 @@ $cabecera = '
 ';
 include 'header.php';
 ?>
+<div class="formulario">
+     
+    <table class="form">
+        <tbody>
+            <tr>
+                <td class="label">
+                    <label>Producto</label>
+                </td>
+                <td class="input">
+                    
+                    <select>
 
+					</select>
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Sub Producto</label>
+                </td>
+                <td class="input">
+                    <select>
+
+					</select>
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Agencia de Solicitud :</label>
+                </td>
+                <td class="input">
+                    <select>
+
+					</select>
+                </td>
+            </tr>
+            
+            <tr>
+                <td class="label">
+                    <label>Monto</label>
+                </td>
+                <td class="input">
+                    <input type="text" class="numerico" >
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Moneda</label>
+                </td>
+                <td class="input">
+                    
+                    <table border="0">
+	<tbody><tr>
+		<td><label><input name="moneda" type="radio" value="1" checked="checked">Soles</label></td><td><label><input name="moneda" type="radio" value="2">Dólares</label></td>
+	</tr>
+</tbody></table>
+                </td>
+            </tr>
+            <tr>
+                <td class="label">
+                    <label>Plazo</label>
+                </td>
+                <td class="input">
+                    <input type="text" class="numerico" onkeypress="return isNumberKey(event)">
+                    &nbsp;días
+                </td>
+            </tr>
+            <tr>
+                <td align="right" colspan="2">
+                   &nbsp;
+                </td>
+            </tr>
+            <tr>
+                <td align="right" colspan="2" style="padding-right: 35px;">
+                    <input type="button" value="Imprimir" class="button" onclick="window.print();">
+                    <input type="submit" value="Calcular" class="button">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" class="titulo">
+                    <span>PROGRAMA DE DEPOSITOS</span>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <table class="form">
+                        <tbody><tr>
+                            <td class="label">
+                                <label>Interés Generado</label>
+                            </td>
+                            <td class="input">
+                                <input type="text" readonly="true">
+                            </td>
+                            <td class="label">
+                                <label>Tasa Efectiva Anual</label>
+                            </td>
+                            <td class="input">
+                                <input type="text" readonly="true">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="label">
+                                <label>ITF</label>
+                            </td>
+                            <td class="input">
+                                <input type="text" readonly="true">
+                            </td>
+                            <td class="label">
+                                <label>Año Base</label>
+                            </td>
+                            <td class="input">
+                                <input type="text" value="" readonly="true">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="label">
+                                <label>Monto total a pagar</label>
+                            </td>
+                            <td class="input">
+                                <input type="text" readonly="true">
+                            </td>
+                            <td class="label">
+                                <label>Impuesto Transacciones Financiera</label>
+                            </td>
+                            <td class="input">
+                                <input type="text" value="" readonly="true">
+                            </td>
+                        </tr>
+                    </tbody></table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+            </div>
 <?php
 include 'footer.php';
 ?>
