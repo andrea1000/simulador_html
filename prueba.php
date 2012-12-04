21813cb7b83ca8dd76419dbb56d7bdc632497062
Vladimir <vladimir_11_17@hotmail.com>
2012-12-04 17:29:11 -0500

    Xml con JQuery

--- prueba.php
+++ prueba.php
@@ -1,5 +1,5 @@
 <?php
-	if (file_exists('data.xml'))
+	/*if (file_exists('data.xml'))
 	{
 		$xml = simplexml_load_file("data.xml");
 	}
@@ -11,5 +11,30 @@
 	foreach($xml->parametrica->registro as $child)
 	{
 		echo $child->getName() . "->" . $child->nParCodigo . "<br>";
-	}
+	}*/
+?>
+<?php
+$cabecera = '<script src="Script/Script.js" type="text/javascript"></script>';
+include 'header.php';
+?>
+
+<script>
+
+$.get("data.xml", function (xml) {
+	$(xml).find("parametrica").each(function () {
+	$(this).find("registro").each(function () {
+       var name = $(this).find('nParCodigo').text();
+ 
+       $("#display").append(name+"<br/>");
+	});
+
+    });
+    
+});
+
+</script>
+<div id="display">
+</div>
+<?php
+include 'footer.php';
 ?>
